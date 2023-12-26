195ce9e4f52dacdc69df20fe436b08fcd28e5a50
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2023-12-26 14:54:42 +0200

    DZ_1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+import { Profile } from './Profile/Profile.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FriendList } from './FriendList/FriendList.jsx';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
+import { Container, Wrapper } from './App.module.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="UPLOAD STATS" stats={data} />
+
+      <Wrapper>
+        <FriendList friends={friends} />
+
+        <TransactionHistory transactions={transactions} />
+      </Wrapper>
+    </Container>
   );
 };
